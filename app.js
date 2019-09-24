cc85950f01b9eba7855f5ca87f0a5d9164dd88f4
qinghe <david@getstore.cn>
2019-09-24 17:23:34 +0800

    always load babel-register

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ require('dotenv').config();
 
 const env = process.env.NODE_ENV || 'development';
 const port = process.env.PORT || 3000;
-const src = env === 'production' ? './build/index' : './src/index';
+//const src = env === 'production' ? './build/index' : './src/index';
 
 require('babel-polyfill');
-if (env === 'development') { require('babel-register'); }
+require('babel-register');
 
-const app = require(src).default;
+const app = require('./src/index').default;
 
 //Here we're assigning the server to a variable because
 //we're going to want to manually rip down the server in testing

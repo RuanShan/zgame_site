f45add60ad93367ee4693f6061d8fb400cce6d70
qinghe <david@getstore.cn>
2019-09-25 22:32:16 +0800

    修正手机版导航菜单

--- src/config/game.js
+++ src/config/game.js
@@ -1,5 +1,6 @@
 module.exports = {
 
-  urlBase: process.env.GAME_URL_BASE || 'https://testwx.natapp4.cc'
+  urlBase: process.env.GAME_URL_BASE || 'https://testwx.natapp4.cc',
+  backendUrl: process.env.GAME_BACKEND_URL||'http://127.0.0.1:8081'
 
 }

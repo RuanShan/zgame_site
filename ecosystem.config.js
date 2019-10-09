ace54c45f9e4a8f721fcc4d00800f6c23ddbc26a
qinghe <david@getstore.cn>
2019-10-09 09:40:21 +0800

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps : [
+          {
+          "name": "zgame_site",
+          "script": "app.js",
+          "instances": 1,
+          "exec_mode": "fork",
+      }
+  ]
+};
